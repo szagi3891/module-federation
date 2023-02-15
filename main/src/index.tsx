d54a855489744076e24c6d43927cfdbadad04342
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2023-02-15 07:51:37 +0100

    Notatki

--- main/src/index.tsx
+++ main/src/index.tsx
@@ -4,22 +4,22 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { registerNetworRequest, whenIddle } from "./utils/Idle";
 
-const createJsx = (setBodyFlag: boolean): React.ReactElement => {
+const createJsx = (setBody: boolean): React.ReactElement => {
     const cssContainer = document.createElement('div');
 
     const myCache = createCache({
-        key: setBodyFlag ? 'emotion-fake' : 'emotion-client',
+        key: setBody ? 'emotion-fake' : 'emotion-client',
         container: cssContainer
     });
 
-    const setBody = (body: HTMLElement | null) => {
+    const setBodyFn = setBody ? (body: HTMLElement | null) => {
         if (body !== null) {
             console.info('!!!!! add container');
             body.appendChild(cssContainer);
         }
-    };
+    } : undefined;
 
-    return <App setBody={setBodyFlag ? setBody : undefined} myCache={myCache} />
+    return <App setBody={setBodyFn} myCache={myCache} />
 };
     
 const fakeRoot = ReactDOM.createRoot(document.createElement('html'));
@@ -38,22 +38,9 @@ whenIddle(() => {
     fakeRoot.unmount();
 });
 
-// .hydrateRoot(document.documentElement, jsx, {
-//     onRecoverableError: (error) => {
-//         console.info('Przechwycony komunikat o błędzie', error);
-//     }
-// });
-// .createRoot(root).render(<App />)
-
-
-
-//TODO - zrobić test, spróbować wyrenderować <App/> z jakimś lazy-loadingiem, i sprawdzić w jakiej formie wygeneruje się wynikowy html
+//TODO - przekazywać funkcję zwrotną do App, za pomocą której aplikacja będzie mogła pobrać obecnego urla na którym się znajduje
+//TODO - zorbić podobnie z pushHistory oraz z hitoryChange callbackiem ? Zrobić coś w rodzaju wirtualnego drivera do środowiska
 
 //TODO - trzeba zastanowić się jakie wersje bibliotek są potrzebne w websie, react + mobx
 
-//TODO - Gdy rzucany jest wyjątek w renderze. Co się dzieje w mobx ? Czy wycieka wtedy subskrybcja ?
-
 //TODO - zbadać temat: https://github.com/mobxjs/mobx/issues/2526
-
-// <React.StrictMode>
-// </React.StrictMode>
